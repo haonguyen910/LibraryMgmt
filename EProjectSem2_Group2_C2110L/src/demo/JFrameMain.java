cd2c3c63761b47ade038bdce5f7fc2d2525ec3f8
haonguyen910 <haodev910@gmail.com>
2023-01-05 11:34:46 +0700

    First Commit

---
+++ EProjectSem2_Group2_C2110L/src/demo/JFrameMain.java
@@ -0,0 +1,49 @@
+package demo;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import java.awt.Font;
+
+public class JFrameMain extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JFrameMain frame = new JFrameMain();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public JFrameMain() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 535, 474);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Library Management");
+		lblNewLabel.setBounds(96, 69, 307, 90);
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 26));
+		contentPane.add(lblNewLabel);
+	}
+}
