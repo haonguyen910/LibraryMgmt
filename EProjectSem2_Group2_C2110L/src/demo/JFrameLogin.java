6ba0cba41eb2a283754635c8409ef6281791601c
haodev910 <haodev910@gmail.com>
2023-01-06 08:28:08 +0700

    Layout

---
+++ EProjectSem2_Group2_C2110L/src/demo/JFrameLogin.java
@@ -0,0 +1,47 @@
+package demo;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.border.EmptyBorder;
+
+public class JFrameLogin extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JFrameLogin frame = new JFrameLogin();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public JFrameLogin() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+	}
+
+}
--- EProjectSem2_Group2_C2110L/src/demo/JFrameMain.java
+++ EProjectSem2_Group2_C2110L/src/demo/JFrameMain.java
@@ -5,9 +5,12 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import java.awt.GridLayout;
-import javax.swing.JLabel;
-import java.awt.Font;
+import java.awt.BorderLayout;
+import javax.swing.JTabbedPane;
+import javax.swing.UIManager;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class JFrameMain extends JFrame {
 
@@ -17,6 +20,11 @@ public class JFrameMain extends JFrame {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -34,16 +42,45 @@ public class JFrameMain extends JFrame {
 	 */
 	public JFrameMain() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 535, 474);
+		setBounds(100, 100, 900, 500);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnNewMenu = new JMenu("Action");
+		menuBar.add(mnNewMenu);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("Issue Book");
+		mnNewMenu.add(mntmNewMenuItem);
+		
+		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Return Book");
+		mnNewMenu.add(mntmNewMenuItem_1);
+		
+		JMenu mnNewMenu_1 = new JMenu("Account");
+		menuBar.add(mnNewMenu_1);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		contentPane.setLayout(new BorderLayout(0, 0));
 		
-		JLabel lblNewLabel = new JLabel("Library Management");
-		lblNewLabel.setBounds(96, 69, 307, 90);
-		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 26));
-		contentPane.add(lblNewLabel);
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		contentPane.add(tabbedPane, BorderLayout.CENTER);
+		
+		JPanel jpanelBookList = new JPanel();
+		tabbedPane.addTab("Book List", null, jpanelBookList, null);
+		
+		JPanel jpanelIssue = new JPanel();
+		tabbedPane.addTab("Issue Register", null, jpanelIssue, null);
+		
+		JPanel jpanelStatistic = new JPanel();
+		tabbedPane.addTab("Statistic Report", null, jpanelStatistic, null);
+		
+		JPanel jpanelUserList = new JPanel();
+		tabbedPane.addTab("Customer List", null, jpanelUserList, null);
+		
+		JPanel panel = new JPanel();
+		tabbedPane.addTab("Employee List", null, panel, null);
 	}
+
 }
